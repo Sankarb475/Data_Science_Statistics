77203c07bb059baf1a8c387bfb7494268279c67c
Sankar Biswas <sankarb475@gmail.com>
2019-03-29 18:36:05 +0530

    Create DataManipulationUsingPandas.py

---
+++ Pandas/DataManipulationUsingPandas.py
@@ -0,0 +1,26 @@
+The three phases of data manipulation :: 
+1) Data Preparation
+2) Data Transformation
+3) Data Aggregation
+
+
+# Data Preparation
+==============================================================================================================================
+Before you start manipulating data, it is necessary to prepare the data and assemble them in the form of data structures such 
+that they can be manipulated later with the tools made available by the pandas library.
+
+The different procedures for data preparation are :: 
+a) Loading
+b) assembling
+    • Merging 
+    • Concatenating
+    • Combining
+    
+c) Reshaping (pivoting)
+d) Removing
+
+We will see how to perform all the operations necessary to get the data into a unified data structure.
+
+# Merging ::
+The merging operation, which corresponds to the JOIN operation consists of a combination of data through the connection of 
+rows using one or more keys
