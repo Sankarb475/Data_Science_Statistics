4270a7d021533331c6d62ae38774192378c5c045
Sankar Biswas <sankarb475@gmail.com>
2019-11-14 13:21:21 +0530

    adding multi-sheet writing in an excel

--- Pandas/excelmanipulation.py
+++ Pandas/excelmanipulation.py
@@ -10,3 +10,26 @@ for f in glob.glob("C:/Users/sbiswas149/Applications/data/PoC data/Australia/Fin
     all_data = all_data.append(df,ignore_index=True)
     
 all_data.to_excel("C:/Users/sbiswas149/Applications/data/PoC data/Australia/output.xlsx")
+
+
+
+#adding multiple sheet into a single excel file
+import glob
+import pandas as pd
+
+all_data1 = pd.DataFrame()
+all_data2 = pd.DataFrame()
+print("hello")
+for f in glob.glob("C:/Users/sbiswas149/Applications/data/PoC data/Australia/Final Output/*"):
+    df1 = pd.read_excel(f, header=[0,1], sheet_name = 'Details')
+    df2 = pd.read_excel(f, header=[0], sheet_name = "Log File")
+    all_data1 = all_data1.append(df1, ignore_index=True)
+    all_data2 = all_data2.append(df2, ignore_index=True)
+    
+with pd.ExcelWriter('C:/Users/sbiswas149/Applications/data/PoC data/Australia/output.xlsx') as writer:
+    all_data1.to_excel(writer, sheet_name='Details')
+    all_data2.to_excel(writer, sheet_name='Log File')
+    
+#all_data1.to_excel("C:/Users/sbiswas149/Applications/data/PoC data/Australia/output.xlsx", sheet_name = "Details")
+#all_data2.to_excel("C:/Users/sbiswas149/Applications/data/PoC data/Australia/output.xlsx", sheet_name = "Log File")
+print("done")
